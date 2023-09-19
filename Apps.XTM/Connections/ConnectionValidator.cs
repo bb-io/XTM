5c44a49c7ed9c0f920fb69993e7a8ac85567677f
bZverok <bohdan.zverok@blackbird.io>
2023-09-19 14:44:53 +0300

    added connection validator

---
+++ Apps.XTM/Connections/ConnectionValidator.cs
@@ -0,0 +1,34 @@
+﻿using Apps.XTM.Constants;
+using Apps.XTM.RestUtilities;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+using RestSharp;
+
+namespace Apps.XTM.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    private XTMClient Client => new();
+    
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var response = await Client.ExecuteXtmWithJson(ApiEndpoints.System,
+                Method.Get,
+                null,
+                authProviders.ToArray());
+
+            return new() { IsValid = true };
+        }
+        catch (Exception ex)
+        {
+            return new()
+            {
+                IsValid = false,
+                Message = ex.Message
+            };
+        }
+    }
+}
