0283ff007a0719dfa827575ccbdc845156d68ecc
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-01-31 18:16:34 +0200

    Added Delivery date as DateTime in ProjectEstimates

--- Apps.XTM/Models/Response/Projects/ProjectEstimates.cs
+++ Apps.XTM/Models/Response/Projects/ProjectEstimates.cs
@@ -4,13 +4,19 @@ namespace Apps.XTM.Models.Response.Projects;
 
 public class ProjectEstimates
 {
-    [Display("Project ID")] public string ProjectId { get; set; }
+    [Display("Project ID")] 
+    public string ProjectId { get; set; }
 
     public double Price { get; set; }
 
-    [Display("Tax price")] public double TaxPrice { get; set; }
+    [Display("Tax price")] 
+    public double TaxPrice { get; set; }
 
-    [Display("Delivery date in UNIX")] public long DeliveryDate { get; set; }
+    [Display("Delivery date in UNIX")] 
+    public long DeliveryDate { get; set; }
+
+    [Display("Delivery date in DateTime")] 
+    public DateTime DeliveryDateFormatted => DateTimeOffset.FromUnixTimeMilliseconds(DeliveryDate).DateTime;
 
     public string Currency { get; set; }
 }
