f8a989367849be0da70f5de8ebe1cb2c157ebbf8
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-02-09 12:40:10 +0200

    Updated Get project users action

--- Apps.XTM/Actions/ProjectActions.cs
+++ Apps.XTM/Actions/ProjectActions.cs
@@ -265,10 +265,18 @@ public class ProjectActions : XtmInvocable
         var endpoint = $"{ApiEndpoints.Projects}/{project.ProjectId}{ApiEndpoints.Users}";
         var projectUsers = await Client.ExecuteXtmWithJson<ProjectUsers>(endpoint, Method.Get, null, Creds);
         
-        var projectUsersResponse = new ProjectUsersResponse();
+        if(projectUsers.ProjectManager is null)
+            throw new Exception("Project manager not found");
         
-        projectUsersResponse.ProjectManager = await GetUserById(projectUsers.ProjectManager.UserId);
-        projectUsersResponse.ProjectCreator = await GetUserById(projectUsers.ProjectCreator.UserId);
+        if(projectUsers.ProjectCreator is null)
+            throw new Exception("Project creator not found");
+        
+        var projectUsersResponse = new ProjectUsersResponse
+        {
+            ProjectManager = await GetUserById(projectUsers.ProjectManager.UserId),
+            ProjectCreator = await GetUserById(projectUsers.ProjectCreator.UserId)
+        };
+
         foreach (var linguist in projectUsers.Linguists)
         {
             projectUsersResponse.Linguists.Add(await GetUserById(linguist.UserId));
--- Apps.XTM/Models/Response/User/ProjectUsers.cs
+++ Apps.XTM/Models/Response/User/ProjectUsers.cs
@@ -1,8 +1,12 @@
-﻿namespace Apps.XTM.Models.Response.User;
+﻿using Newtonsoft.Json;
+
+namespace Apps.XTM.Models.Response.User;
 
 public class ProjectUsers
 {
     public List<ProjectUser> Linguists { get; set; }
+    
     public ProjectUser ProjectManager { get; set; }
+    
     public ProjectUser ProjectCreator { get; set; }
 }
