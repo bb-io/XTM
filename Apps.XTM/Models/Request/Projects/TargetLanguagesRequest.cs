1340ea53a86801ce2ed592f1e6d7567e22b4506d
GrinchArt <artemryabushenko@gmail.com>
2024-11-22 18:15:12 +0200

    Made target languages nullable in Get project metrics 5

--- Apps.XTM/Models/Request/Projects/TargetLanguagesRequest.cs
+++ Apps.XTM/Models/Request/Projects/TargetLanguagesRequest.cs
@@ -8,5 +8,5 @@ public class TargetLanguagesRequest
 {
     [Display("Target languages")]
     [DataSource(typeof(LanguageDataHandler))]
-    public IEnumerable<string> TargetLanguages { get; set; }
+    public IEnumerable<string>? TargetLanguages { get; set; }
 }
