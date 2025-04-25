2216736bea38fb2c59e795fc6a9d3e5d13dc7dcd
ce-nistal <cecilia.nistal@blackbird.io>
2025-04-24 22:18:47 -0300

    Test polling

--- Apps.XTM/Polling/PollingList.cs
+++ Apps.XTM/Polling/PollingList.cs
@@ -102,7 +102,7 @@ public class PollingList(InvocationContext invocationContext) : XtmInvocable(inv
             }
         }
 
-        if (!String.IsNullOrEmpty(projectOptionalRequest.CustomerNameContains))
+        if (!String.IsNullOrEmpty(projectOptionalRequest.CustomerNameContains) && result.Result?.Projects != null)
         {
             var filteredProjects = new List<SimpleProject>();
             foreach (var project in result.Result?.Projects)
@@ -114,7 +114,7 @@ public class PollingList(InvocationContext invocationContext) : XtmInvocable(inv
                 }
             }
             
-            if (filteredProjects != null && filteredProjects.Count > 0)
+            if  (filteredProjects.Count > 0)
             {
                 result.Result = new(filteredProjects);
             }
