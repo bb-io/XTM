65fc36f7b42def99703b3e4f5844b6f55618d18a
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-12-11 13:13:05 +0200

    remove duplicated FileTests, move everything to FileActionsTests

--- Tests.XTM/FileActionsTests.cs
+++ Tests.XTM/FileActionsTests.cs
@@ -1,6 +1,7 @@
 ﻿using Tests.XTM.Base;
 using Apps.XTM.Actions;
 using Apps.XTM.Models.Request.Files;
+using Apps.XTM.Models.Response.Files;
 using Apps.XTM.Models.Request.Projects;
 using Blackbird.Applications.Sdk.Common.Files;
 using Blackbird.Applications.Sdk.Common.Invocation;
@@ -85,6 +86,47 @@ public class FileActionsTests : TestBaseMultipleConnections
 
         // Assert
         PrintResult(response);
+        Assert.IsNotNull(response);
+    }
+
+    [ContextDataSource, TestMethod]
+    public async Task DownloadTranslatedFiles_IsSuccess(InvocationContext context)
+    {
+        // Arrange
+        var action = new FileActions(context, FileManager);
+        var project = new ProjectRequest { ProjectId = "69721875" };
+        var fileGenerate = new DownloadTranslationsRequest { };
+
+        // Act
+        var response = await action.DownloadTranslations(project, fileGenerate);
+
+        // Assert
+        int i = 0;
+        foreach (var file in response.Files)
+        {
+            if (file.FileDescription is XtmProjectFileDescription projectFile)
+            {
+                TestContext.WriteLine($"[{i++}] File name: {projectFile.FileName}");
+                TestContext.WriteLine($"    File ID: {projectFile.FileId}");
+                TestContext.WriteLine($"    Job ID: {projectFile.JobId}");
+                TestContext.WriteLine($"    Target Language: {projectFile.TargetLanguage}");
+            }
+        }
+        Assert.IsNotNull(response);
+    }
+
+    [ContextDataSource, TestMethod]
+    public async Task DownloadProjectFiles_IsSuccess(InvocationContext context)
+    {
+        // Arrange
+        var action = new FileActions(context, FileManager);
+        var project = new ProjectRequest { ProjectId = "107731759" };
+        var fileGenerate = new DownloadProjectFileRequest { FileScope = "JOB", FileId = "107965948" };
+
+        // Act
+        var response = await action.DownloadProjectFile(project, fileGenerate);
+
+        // Assert
         Assert.IsNotNull(response);
     }
 }
